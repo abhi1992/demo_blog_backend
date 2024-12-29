354f89ae6231ca30877d3e91fe700fc499d13f5d
Abhishek Banerjee <abhishekbanerjee1992@gmail.com>
2024-12-29 18:33:24 +0530

    first commit

---
+++ src/main/java/com/purpleteaches/blog/entities/User.java
@@ -0,0 +1,31 @@
+package com.purpleteaches.blog.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(name = "username", nullable = false)
+	private String user_name;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	private String email;
+
+	private String password;
+
+}
